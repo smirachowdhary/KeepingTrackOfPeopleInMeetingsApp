f9a1c3ddf30a5ba7136dfa662a2b26b3ff4263e3
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-14 12:05:08 -0800

    command prompt interface, with lists, no dicts

--- Command_line_2nd_try.py
+++ Command_line_2nd_try.py
@@ -1,59 +1,28 @@
 import random
 
-dictionary  =  {
-        1   : "LEONIS",
-        2   : "BRIAN",
-        3   : "TYLER",
-        4   : "RACHEL",
-        5   : "SMIRA",
-        6   : "ISAAC",
-        7   : "THOMAS",
-        8   : "ALLISON",
-        9   : "YANA",
-        10  : "SAMUEL",
-        11  : "AIDAN",
-        12  : "NATALIE",
-        13  : "ALI",
-        14  : "JONATHAN",
-        15  : "AZIZA",
-        16  : "MICHAELA",
-        17  : "ALISTAIR",
-        18  : "PARAMEE",
-        19  : "EVELYN",
-        20  : "MAYA",
-        21  : "ISABEL",
-        22  : "ARIA",
-        23  : "XAVIER",
-        24  : "ALYSSA",
-        25  : "NATHAN",
-        26  : "DARREN",
-}
+students_status = []
+students_name = ["LEONIS", "BRIAN", "TYLER", "RACHEL", "SMIRA", "ISAAC", "THOMAS", "ALLISON", "YANA", "SAMUEL",
+ "AIDAN", "NATALIE", "ALI", "JONATHAN", "AZIZA", "MICHAELA", "ALISTAIR", "PARAMEE", "EVELYN", "MAYA", "ISABEL",
+ "ARIA", "XAVIER", "ALYSSA", "NATHAN", "DARREN",
+]
+students_info = []
 
 i = 0
 while i < 26:
-    random_number = random.randint(1,26-i)
-    print(dictionary[random_number])
+    random_number = random.randint(1,26)
+    print(students_name[random_number])
     j = 0
     while j < 26:
         if j+1 == random_number:
-                p = input("Present and answered question:")
-                if p == "Y" or p == "y":
-                        student = dictionary[random_number]
-                        dictionary[random_number] = "present and has answered question."
-                        print(student, "is", dictionary[random_number])
-                        j+=1
-                        continue
-                a = input("Absent:")
-                if a == "Y" or a == "y":
-                        student = dictionary[random_number]
-                        dictionary[random_number] = "absent."
-                        print(student, "is", dictionary[random_number])
-                        j+=1
-                        continue
-                n = input("Here but not here:")
-                if n == "Y" or n == "y":
-                        student = dictionary[random_number]
-                        dictionary[random_number] = "here but not here."
-                        print(student, "is", dictionary[random_number])
+                student_status = input("""
+                Present and answered question:
+                Absent:
+                Not attentive:
+                """)
+                students_status.append(student_status)
+                students_info.append(students_status)
+                students_info.append(students_name)
         j+=1
     i+=1
+
+print(students_info)
