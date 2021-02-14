42b9ca408d88f95c94da79962ef3bee8e791b6dd
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-14 12:32:16 -0800

    creating a file that will solve a problem that I've been having: repitation names

---
+++ Command_line_4th_try_solving_repitation.py
@@ -0,0 +1,31 @@
+import random
+
+students_info = []
+students_name = ["LEONIS", "BRIAN", "TYLER", "RACHEL", "SMIRA", "ISAAC", "THOMAS", "ALLISON", "YANA", "SAMUEL",
+ "AIDAN", "NATALIE", "ALI", "JONATHAN", "AZIZA", "MICHAELA", "ALISTAIR", "PARAMEE", "EVELYN", "MAYA", "ISABEL",
+ "ARIA", "XAVIER", "ALYSSA", "NATHAN", "DARREN"]
+go_on = True
+while go_on:
+    random_number = random.randint(0,len(students_name)-1)
+    print(students_name[random_number])
+    student_name = students_name[random_number]
+    teacher_input = input("""
+    Present and answered question:
+    Absent:
+    Not attentive:
+    Show:
+    Exit:
+    """)
+    teacher_input = teacher_input.lower()
+    if  teacher_input == "p" or  teacher_input == "a" or  teacher_input == "n":
+        student_info = [student_name,  teacher_input]
+        students_info.append(student_info)
+    if  teacher_input == "s":
+        print(students_info)
+        temp_go_on = input("Do you want to continue?")
+        if temp_go_on == "y" or temp_go_on == "Y":
+            go_on = True
+        else:
+            go_on = False
+    if teacher_input == "e":
+        go_on = False
