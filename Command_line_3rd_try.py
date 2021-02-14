fa892967ec55351d0b5c3150e3dca52a442e2507
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-14 12:05:20 -0800

    less arrays, nicer

--- Command_line_3rd_try.py
+++ Command_line_3rd_try.py
@@ -1,28 +1,31 @@
 import random
 
-students_status = []
+students_info = []
 students_name = ["LEONIS", "BRIAN", "TYLER", "RACHEL", "SMIRA", "ISAAC", "THOMAS", "ALLISON", "YANA", "SAMUEL",
  "AIDAN", "NATALIE", "ALI", "JONATHAN", "AZIZA", "MICHAELA", "ALISTAIR", "PARAMEE", "EVELYN", "MAYA", "ISABEL",
- "ARIA", "XAVIER", "ALYSSA", "NATHAN", "DARREN",
-]
-students_info = []
-
-i = 0
-while i < 26:
-    random_number = random.randint(1,26)
+ "ARIA", "XAVIER", "ALYSSA", "NATHAN", "DARREN"]
+go_on = True
+while go_on:
+    random_number = random.randint(0,len(students_name)-1)
     print(students_name[random_number])
-    j = 0
-    while j < 26:
-        if j+1 == random_number:
-                student_status = input("""
-                Present and answered question:
-                Absent:
-                Not attentive:
-                """)
-                students_status.append(student_status)
-                students_info.append(students_status)
-                students_info.append(students_name)
-        j+=1
-    i+=1
-
-print(students_info)
+    student_name = students_name[random_number]
+     teacher_input = input("""
+    Present and answered question:
+    Absent:
+    Not attentive:
+    Show:
+    Exit:
+    """)
+    if  teacher_input == p or  teacher_input == P or  teacher_input == a or  teacher_input == A or  teacher_input == n or  teacher_input == N:
+        student_info = [student_name,  teacher_input]
+        students_info.append(student_info)
+    if  teacher_input == S or  teacher_input == s:
+        print(students_info)
+        print("Do you want to continue?")
+        temp_go_on = input()
+        if temp_go_on == y or temp_go_on == Y:
+            go_on = False
+        else:
+            go_on = True
+    if teacher_input == e or teacher_input == E:
+        go_on = False
