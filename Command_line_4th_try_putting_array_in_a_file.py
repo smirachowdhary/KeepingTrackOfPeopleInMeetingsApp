343f46d97fa52a40056493b4cda7ebaa3124a0f6
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-17 09:17:56 -0800

    making it cleaner- output and the code-by adding enters

--- Command_line_4th_try_putting_array_in_a_file.py
+++ Command_line_4th_try_putting_array_in_a_file.py
@@ -11,6 +11,7 @@ go_on = True
 while go_on:
     random_number = random.randint(0,len(students_name)-1)
     student_name = students_name[random_number]
+    
     if len(students_name)-1 == len(students_info)-1:
         print(f"""
         For this meeting, all names have been exhausted.
@@ -21,6 +22,7 @@ while go_on:
             print(f"{students_info[i][0]} is {students_info[i][1]}.")
             i+=1
         break
+    
     if student_name in students_processed:
         continue
 
@@ -38,6 +40,7 @@ while go_on:
         student_info = [student_name,  teacher_input]
         students_info.append(student_info)
         students_processed.append(student_name)
+    
     elif  teacher_input == "s":
         print(students_info)
         temp_go_on = input("Do you want to continue?")
@@ -46,8 +49,10 @@ while go_on:
             go_on = True
         else:
             go_on = False
+    
     elif teacher_input == "e":
         go_on = False
+    
     else:
         print("You did not answer with PANSE, so try again. This try will not be shown.")
         go_on = True
@@ -55,11 +60,15 @@ while go_on:
 dectory = input("Enter a path to a new file here(word prefrebly).")
 print("Opening the file...")
 target = open(dectory, "w")
+
 print("Clearing everything that is in the new file. Goodbye.")
 target.truncate()
+
 print("I am going to write the students' info in the new file.")
+
 meeting = input("What meeting is this?")
 target.write(f"{meeting} Meeting Info:\n\n\n\n")
+
 i=0
 while i < 26:
     target.write(f"{students_info[i][0]} is {students_info[i][1]}.")
@@ -68,4 +77,5 @@ while i < 26:
 
 print("And fianally, we close it.")
 target.close()
-print("It is done. Open that file!")
+
+print("It is done. Open that file to see!")
