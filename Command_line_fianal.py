753c127bb963e19869847105bed2c17b6215d541
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-23 08:26:48 -0800

    not so confusing file names

--- Command_line_fianal.py
+++ Command_line_fianal.py
@@ -2,28 +2,27 @@ import random
 from sys import argv
 from os.path import exists
 
-students_info = []
-students_processed=[]
-students_name = ["LEONIS", "BRIAN", "TYLER", "RACHEL", "SMIRA", "ISAAC", "THOMAS", "ALLISON", "YANA", "SAMUEL",
+info_list = []
+processed_list=[]
+name_list = ["LEONIS", "BRIAN", "TYLER", "RACHEL", "SMIRA", "ISAAC", "THOMAS", "ALLISON", "YANA", "SAMUEL",
  "AIDAN", "NATALIE", "ALI", "JONATHAN", "AZIZA", "MICHAELA", "ALISTAIR", "PARAMEE", "EVELYN", "MAYA", "ISABEL",
  "ARIA", "XAVIER", "ALYSSA", "NATHAN", "DARREN"]
 go_on = True
 while go_on:
-    random_number = random.randint(0,len(students_name)-1)
-    student_name = students_name[random_number]
-    
-    if len(students_name)-1 == 25:
+    random_number = random.randint(0,len(name_list)-1)
+    student_name = name_list[random_number]
+    if len(processed_list)-1 == 25:
         print(f"""
         For this meeting, all names have been exhausted.
         There is the info:
         """)
         i = 0
         while i < 26:
-            print(f"{students_info[i][0]} is {students_info[i][1]}.")
+            print(f"{info_list[i][0]} is {info_list[i][1]}.")
             i+=1
         break
     
-    if student_name in students_processed:
+    if student_name in processed_list:
         continue
 
     print(student_name)
@@ -38,11 +37,11 @@ while go_on:
 
     if  teacher_input == "p" or  teacher_input == "a" or  teacher_input == "n":
         student_info = [student_name,  teacher_input]
-        students_info.append(student_info)
-        students_processed.append(student_name)
+        info_list.append(student_info)
+        processed_list.append(student_name)
     
     elif  teacher_input == "s":
-        print(students_info)
+        print(info_list)
         temp_go_on = input("Do you want to continue?")
         temp_go_on = temp_go_on.lower()
         if temp_go_on == "y":
@@ -71,7 +70,7 @@ target.write(f"{meeting} Meeting Info:\n\n\n\n")
 
 i=0
 while i < 26:
-    target.write(f"{students_info[i][0]} is {students_info[i][1]}.")
+    target.write(f"{info_list[i][0]} is {info_list[i][1]}.")
     target.write("")
     i+=1
 
