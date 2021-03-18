ee6c0a4938dd9fc8973d945d21f144d3745a4c1d
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-03-18 09:20:17 -0700

    starting docx

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 import student
+import studentList
 
 s1 = student.Student()
 
@@ -9,11 +10,16 @@ window = tk.Tk(className = "My Program")
 nameLbl = tk.Label(text = "Selected Name:")
 nameLbl.pack()
 
-tmp=""
-for i in range(0,25):
-    tmp += s1.get_random_name() + "\n "
-
-showNameLbl = tk.Label(text = tmp,fg="blue",bg="#66FFCC", width=50)
+s1.get_random_name()
+showNameLbl = tk.Label(text = s1.get_current_name())
 showNameLbl.pack()
+
+listsFrame = tk.Frame()
+
+sl1 = studentList.StudentList(listsFrame,"Present", s1, showNameLbl)
+sl2 = studentList.StudentList(listsFrame,"Absent", s1, showNameLbl)
+sl3 = studentList.StudentList(listsFrame,"Needs Attention", s1, showNameLbl)
+
+listsFrame.pack()
 
 window.mainloop()
